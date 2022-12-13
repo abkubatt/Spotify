94fa1c43db1b0f92538933065fb8bf97a73d09a2
abkubatt <abkubatt@gmail.com>
2022-12-14 00:16:08 +0600

    Initial Commit

---
+++ Spotify/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Abdulmajit Kubatbekov on 13.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+
